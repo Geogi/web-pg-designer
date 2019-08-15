7cade08c7f288ba2cceb23be4229119a1a8c05c7
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2019-08-15 18:43:12 +0200

    initial

---
+++ src/components/hello.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+
+export const Hello = () => <h1>test</h1>;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import {Hello} from "./components/hello";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <Hello/>,
+    document.getElementById("root")
+);
