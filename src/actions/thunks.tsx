31f74d13ffa6201fc3135b9b0f7892fd303e8994
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2019-08-20 00:27:13 +0200

    all ground work for connecting... except node-postgres is only for node
    obviously. will build a server then.

---
+++ src/actions/thunks.tsx
@@ -0,0 +1,28 @@
+import {createAction} from "../actionReduce";
+import {Root} from "../reducers/root";
+import {databaseErr, databaseOk} from "./actions";
+import {Pool} from "pg";
+
+export const databaseSetup = createAction("DATABASE/SETUP",
+  () => (dispatch: Function) =>
+    dispatch(databaseTry())
+);
+
+const databaseTryAsync = async (pool: Pool) => {
+  const client = await pool.connect();
+  const version: string = (await client.query("SELECT VERSION()")).rows[0];
+  client.release();
+  return version;
+};
+export const databaseTry = createAction("DATABASE/TRY",
+  () => async (dispatch: Function, getState: () => Root) => {
+    const pool = getState().database.pool;
+    if (pool === null) {
+      dispatch(databaseErr("Could not set up the pool."));
+      return;
+    }
+    databaseTryAsync(pool)
+      .then((version: string) => dispatch(databaseOk(version)))
+      .catch((err: string) => dispatch(databaseErr(err)));
+  }
+);
